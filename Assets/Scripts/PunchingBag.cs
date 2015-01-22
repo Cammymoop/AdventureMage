82505a1042d77af81af9ca5b6bc2dde8068f5eca
Charles Burnham <felixtheslayer@gmail.com>
2015-01-21 18:42:48 -0600

    add punching bag

---
+++ Assets/Scripts/PunchingBag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace AdventureMage.Actors
+{
+    public class PunchingBag : MonoBehaviour
+    {
+        [SerializeField] private bool facingRight = true;
+
+        private void Awake()
+        {
+            if (!facingRight) {
+                Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
