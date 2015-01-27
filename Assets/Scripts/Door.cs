49cd6fc6417c9012d4abecfbe46a0f3160464256
Charles Burnham <felixtheslayer@gmail.com>
2015-01-26 21:14:54 -0600

    teleport when going through door instead of switching scenes

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,13 +5,29 @@ namespace AdventureMage.Environment
 {
     public class Door : MonoBehaviour
     {
-        private void Awake()
-        {
+        public string targetScene;
+        private bool canUse = true;
+
+        private void OnTriggerEnter2D(Collider2D obj) {
+            if (CrossPlatformInputManager.GetAxis("Vertical") > 0) {
+                canUse = false;
+            }
         }
 
         private void OnTriggerStay2D(Collider2D obj) {
-            if (obj.gameObject.tag == "Player" && CrossPlatformInputManager.GetAxis("Vertical") > 0) {
-                Application.LoadLevel("insideHouse");
+            if (canUse) {
+                if (obj.gameObject.tag == "Player" && CrossPlatformInputManager.GetAxis("Vertical") > 0) {
+                    foreach (Transform door in transform.parent) {
+                        if (!System.Object.ReferenceEquals(door, transform)) {
+                            obj.gameObject.transform.position = door.position;
+                            GameObject mainCam = GameObject.FindWithTag("MainCamera");
+                            mainCam.BroadcastMessage("centerNow");
+                        }
+                    }
+                }
+            }
+            else if (CrossPlatformInputManager.GetAxis("Vertical") <= 0) {
+                canUse = true;
             }
         }
 
