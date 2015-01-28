e1199e873f16a0a0a7b0e6f58a63ff1aaca0798d
Charles Burnham <felixtheslayer@gmail.com>
2015-01-27 19:53:47 -0600

    imp uses attack animation

---
+++ Assets/Scripts/Imp.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace AdventureMage.Actors
+{
+    public class Imp : MonoBehaviour
+    {
+        [SerializeField] private bool facingRight = true;
+		public int health = 10;
+
+        private impAI AI;
+        private Animator anim;
+        private void Awake()
+        {
+            AI = GetComponent<impAI>();
+            anim = GetComponentInChildren<Animator>();
+            if (!facingRight) {
+                Flip();
+            }
+        }
+
+        public void takeDamage(int dmg)
+        {
+            health -= dmg;
+        }
+
+        private void Flip()
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+        void Update()
+        {
+            anim.SetFloat("targetDistance", Vector3.Distance(transform.position, AI.Target.transform.position));
+            if (health <=  0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
