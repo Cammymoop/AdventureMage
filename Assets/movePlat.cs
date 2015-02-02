c896a47fccd94ba2acaef49a6a2ea674e35fcef7
unknown <Timo Burnham>
2015-02-01 21:59:35 -0600

    updated pathfindign grid

---
+++ Assets/movePlat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlat : MonoBehaviour {
+
+	public float originX;
+	public int platOffSet;
+	public int length;
+	public int speed;
+	void Start () {
+		originX = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Mathf.PingPong((Time.time* speed)+platOffSet , length) + originX, transform.position.y, transform.position.z);
+	}
+}
