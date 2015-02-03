1f627cff71fd86ce6f2eeaf2120e6973935beaaf
unknown <Timo Burnham>
2015-02-03 17:20:25 -0600

    added simple wolf

---
+++ Assets/basicAI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicAI : MonoBehaviour {
+
+	
+	
+		public Transform target;
+		public int moveSpeed;
+		public int maxDistance;
+		
+		
+		private Transform myTransform;
+		
+		void Awake() {
+			myTransform = transform;
+		}    
+		
+	
+		void Start () {
+			GameObject go = GameObject.FindGameObjectWithTag("Player");
+			
+			target = go.transform;
+			
+			maxDistance = 2;
+		}        
+		
+		
+		
+		
+		
+		void FixedUpdate () {
+			
+			
+			if (target.position.x < myTransform.position.x){ myTransform.position -= myTransform.right * moveSpeed * Time.deltaTime;} 
+		else if (target.position.x > myTransform.position.x){ myTransform.position += myTransform.right * moveSpeed * Time.deltaTime;} 
+			
+		
+		}
+		
+	
+	
+}
