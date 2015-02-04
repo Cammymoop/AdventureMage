bbb399e8ae8d8d46d41df6f31abf9f2c60460ebd
Charles Burnham <felixtheslayer@gmail.com>
2015-02-03 20:13:47 -0600

    ai stuff

--- Assets/Scripts/basicAI.cs
+++ Assets/Scripts/basicAI.cs
@@ -1,59 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class basicAI : MonoBehaviour {
+namespace AdventureMage.Actors
+{
+    public class basicAI : MonoBehaviour 
+    {
+        public Transform target;
+        public int moveSpeed;
+        public float minDistance = 0.5f;
+        public float maxDistance = 10f;
 
-		public Transform target;
-		public int moveSpeed;
-		public float minDistance = 0.5f;
-		public float maxDistance = 10f;
-		public int health = 15;
         public bool isRight = true;
 
+        private string curSate = "idle";
         private bool facingRight = true;
-		
-		private Transform myTransform;
-		
-		void Awake() {
-			myTransform = transform;
+        private Transform myTransform;
+
+        void Awake() {
+            myTransform = transform;
             facingRight = isRight;
-		}    
-	
-		void Start () {
-			GameObject go = GameObject.FindGameObjectWithTag("Player");
-			target = go.transform;
-		}        
+        }    
+
+        void Start () {
+            if (!target) {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }        
 	
 		public void takeDamage(int dmg)
-			{
+        {
 			Debug.Log("taking " + dmg);
 			health -= dmg;
-			}
-		
-		void FixedUpdate () {
+        }
+
+        void FixedUpdate () {
             float distance = Mathf.Abs(target.position.x - myTransform.position.x);
             if (distance < maxDistance && distance > minDistance) {
                 if (target.position.x < myTransform.position.x) { 
-                    //myTransform.position -= myTransform.right * moveSpeed * Time.deltaTime;
                     if (facingRight) {
                         Flip();
                     }
                 } else if (target.position.x > myTransform.position.x) {
-                    //myTransform.position += myTransform.right * moveSpeed * Time.deltaTime;
                     if (!facingRight) {
                         Flip();
                     }
-            
-                
                 } 
                 rigidbody2D.velocity = new Vector2(moveSpeed * (facingRight ? 1 : -1), rigidbody2D.velocity.y);
             } else {
                 rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
             }
-				if (health <=  0)
-					{
-					Destroy(gameObject);
-					}
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
 		}
 
         private void Flip() {
@@ -63,4 +62,5 @@ public class basicAI : MonoBehaviour {
             theScale.x *= -1;
             transform.localScale = theScale;
         }
+    }
 }
