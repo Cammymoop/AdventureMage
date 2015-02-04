28659dbf5b6ad3a547edbe2f9e3e951304129600
unknown <Timo Burnham>
2015-02-03 19:18:21 -0600

    gave figher health

--- Assets/Scripts/basicAI.cs
+++ Assets/Scripts/basicAI.cs
@@ -7,7 +7,7 @@ public class basicAI : MonoBehaviour {
 		public int moveSpeed;
 		public float minDistance = 0.5f;
 		public float maxDistance = 10f;
-
+		public int health = 15;
         public bool isRight = true;
 
         private bool facingRight = true;
@@ -23,6 +23,12 @@ public class basicAI : MonoBehaviour {
 			GameObject go = GameObject.FindGameObjectWithTag("Player");
 			target = go.transform;
 		}        
+	
+		public void takeDamage(int dmg)
+			{
+			Debug.Log("taking " + dmg);
+			health -= dmg;
+			}
 		
 		void FixedUpdate () {
             float distance = Mathf.Abs(target.position.x - myTransform.position.x);
@@ -37,11 +43,17 @@ public class basicAI : MonoBehaviour {
                     if (!facingRight) {
                         Flip();
                     }
+            
+                
                 } 
                 rigidbody2D.velocity = new Vector2(moveSpeed * (facingRight ? 1 : -1), rigidbody2D.velocity.y);
             } else {
                 rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
             }
+				if (health <=  0)
+					{
+					Destroy(gameObject);
+					}
 		}
 
         private void Flip() {
